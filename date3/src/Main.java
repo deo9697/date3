92a9a60bb44c64cbf1aa2d06ad792cc327160789
Daniele de Oliveira <164010639+deo9697@users.noreply.github.com>
2024-05-09 17:21:43 +0200

    Update Main.java

--- date3/src/Main.java
+++ date3/src/Main.java
@@ -1,23 +1,15 @@
-import java.time.DayOfWeek;
-import java.time.LocalDateTime;
 
-public class Main {
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Date;
+
+public class Main{
     public static void main(String[] args) {
-        String inputString = "2023-03-01T13:00:00";
-
-        LocalDateTime dateTime = LocalDateTime.parse(inputString);
-
-        int year = dateTime.getYear();
-
-        int month = dateTime.getMonthValue();
-
-        int day = dateTime.getDayOfMonth();
-
-        DayOfWeek dayOfWeek = dateTime.getDayOfWeek();
-
-        System.out.println("Anno: " + year);
-        System.out.println("Mese: " + month);
-        System.out.println("Giorno: " + day);
-        System.out.println("Giorno della settimana: " + dayOfWeek);
+        OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        String dataFull =date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL));
+        System.out.println(dataFull);
     }
 }
+// esercizio svolto con Andrea Vecchione
